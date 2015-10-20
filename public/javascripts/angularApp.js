a6aedd502af508ef7a7dfd974c7d88f97e82511b
Luis Schubert <schubert.luis@me.com>
2015-10-19 21:26:50 -0700

    angular changes fixed some routes you dumbass

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -11,19 +11,60 @@ app.config([
             .state('home', {
                 url: '/home',
                 templateUrl: '/home.html',
-                controller: 'MainCtrl'
+                controller: 'MainCtrl',
+                resolve:{
+                    postPromise: ['posts', function(posts){
+                        return posts.getAll();
+                    }]
+                }
             })
-            .state('posts',{
+            .state('posts', {
                 url: '/posts/{id}',
                 templateUrl: '/posts.html',
-                controller: 'PostsCtrl'
+                controller: 'PostsCtrl',
+                resolve: {
+                    post: ['$stateParams', 'posts', function($stateParams, posts) {
+                        return posts.get($stateParams.id);
+                    }]
+                }
             });
 
         $urlRouterProvider.otherwise('home');
     }]);
-app.factory('posts', [function(){
+
+app.factory('posts', ['$http', function($http){
     var o = {
         posts: []
+    };
+    o.getAll = function() {
+        return $http.get('/posts').success(function(data){
+            angular.copy(data, o.posts);
+        });
+    };
+    o.get = function(id) {
+        return $http.get('/posts/' + id).then(function(res){
+            return res.data;
+        });
+    };
+    o.create = function(post){
+        return $http.post('/posts', post).success(function(data){
+            o.posts.push(data);
+        });
+    }
+    o.upvote = function(post) {
+        return $http.put('/posts/' + post._id + '/upvote')
+            .success(function(data){
+                post.upvotes += 1;
+            });
+    };
+    o.addComment = function(id, comment) {
+        return $http.post('/posts/' + id + '/comments', comment);
+    };
+    o.upvoteComment = function(post, comment) {
+        return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+            .success(function(data){
+                comment.upvotes += 1;
+            });
     };
     return o;
 }]);
@@ -37,44 +78,42 @@ app.controller('MainCtrl', [
 
         $scope.addPost = function(){
             if(!$scope.title || $scope.title === '') { return; }
-            $scope.posts.push({
+            posts.create({
                 title: $scope.title,
                 link: $scope.link,
-                upvotes: 0,
-                comments: [
-                    {author: 'Joe', body: 'cool post!', upvotes:0},
-                    {author: 'bob', body: 'dope post!', upvotes:0}
-                ]
             });
             $scope.title = '';
             $scope.link = '';
         };
 
-        $scope.incrementUpvotesPost = function(post) {
-            post.upvotes += 1;
+        $scope.incrementUpvotes = function(post) {
+            posts.upvote(post);
         };
+        //$scope.incrementUpvotes = function(post) {
+        //    post.upvote(post);
+        //};
     }])
 
 app.controller('PostsCtrl', [
     '$scope',
-    '$stateParams',
     'posts',
-    function($scope, $stateParams, posts){
-        $scope.post = posts.posts[$stateParams.id];
+    'post',
+    function($scope,  posts, post){
+        $scope.post = post;
 
         $scope.addComment = function(){
             if($scope.body === '') { return; }
-            $scope.post.comments.push({
+            posts.addComment(post._id, {
                 body: $scope.body,
                 author: 'user',
-                upvotes: 0
-
+            }).success(function(comment) {
+                $scope.post.comments.push(comment);
             });
             $scope.body = '';
-
         };
-        $scope.incrementUpvotesComment = function(comment) {
-            comment.upvotes += 1;
+
+        $scope.incrementUpvotes = function(comment){
+            posts.upvoteComment(post, comment);
         };
     }
 
